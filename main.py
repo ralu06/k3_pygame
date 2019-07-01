ecbfccd8fce07b871f3524a0382d8b78b333a423
Raul Herrero <RaulHerrero@MacBook-Pro-de-Raul.local>
2019-07-01 22:55:16 +0200

    bolas locas

--- main.py
+++ main.py
@@ -3,17 +3,79 @@ from pygame.locals import *
 import sys #libreria que permite acceder al sistema 
 
 FPS = 60
+'''
+class Ball(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((16, 16)) 
+        self.rect = self.image.get_rect()  #imagen de un rectangulo
+
+        self.x = x
+        self.y = y
+
+        self.color =(255,255,255)
+
+
+        # iniciar la parte grafica
+        self.rect.x = self.x
+        self.rect.y = self.y
+        self.image.fill(self.color)
+
+        
+'''
+
+class Ball(pg.Surface):
+
+    x = 0
+    y = 0
+    dirx = 1
+    diry = 1
+    color = (255,255,255)
+    velocidad = 5
+
+    def __init__(self, x, y):
+        pg.Surface.__init__(self, (16, 16))
+        self.fill((color)
+
+    def avanza (self):
+            #modifica la posicion de ball
+        if x >= 800:
+            self.dirx = -self.velocidad
+        if x <= 0:
+            self.dirx = self.velocidad
+        if y >= 600:
+            self.diry = -self.velocidad
+        if y <= 0:
+            self.diry = self.velocidad  
+    self.x += self.dirx
+    self.y += self.diry
+
 
 class Game:
     clock = pg.time.Clock() #velocidad de refresh
+    VELOCIDAD = 5
     
-    def __init__(self, width, height):
+    def __init__(self, width, height):#crear pantalla
         self.size = (width, height)
-        self.display = pg.display #crear pantalla
+        self.display = pg.display 
         self.screen = self.display.set_mode(self.size)
         self.display.set_caption('Mi juego')
 
+        self.ball = Ball()
+        self.ball1 = Ball()
+        self.ball1.color = (255,0,0)
+        self.ball1.x = 392
+        self.ball1.y =292
+
+        self.ball = pg.Surface((16, 16)) # bola va a ser una superficie
+        self.ball.fill((255, 255, 255))
+
+       
+
     def start(self): #crear bucle
+
+        
+
         while True:
             self.clock.tick(FPS)
 
@@ -21,6 +83,16 @@ class Game:
                 if event.type == QUIT:
                     pg.quit()  #acabar juego
                     sys.exit() #tirar python
+
+            
+            self.ball.avanza()
+            self.ball1.avanza()
+
+            #pintar los sprites en screen
+
+            self.screen.fill((60, 60 , 60))
+            self.screen.blit(self.ball1, (self.ball1.x, self.ball1.y))
+            self.screen.blit(self.ball, (self.ball.x, self.ball.y)) #coloca la imagen pequeña
 
             self.display.flip()
 
---
+++ mainRamon.py
@@ -0,0 +1,83 @@
+import pygame as pg
+from pygame.locals import *
+import sys
+import random
+
+_fps = 60
+
+class Ball(pg.Surface):
+    x = 0
+    y = 0
+    color = (255, 255, 255)
+    velocidad = 5
+    dirx = velocidad
+    diry = velocidad
+
+    def __init__(self):
+        pg.Surface.__init__(self, (16,16))
+        self.fill(self.color)
+
+    def setColor(self, color):
+        self.color = color
+        self.fill(self.color)
+
+    def avanza(self):
+        if self.x >= 800:
+            self.dirx = -self.velocidad
+        if self.x <= 0:
+            self.dirx = self.velocidad
+        if self.y >= 600:
+            self.diry = -self.velocidad
+        if self.y <= 0:
+            self.diry = self.velocidad
+
+        self.x += self.dirx
+        self.y += self.diry
+
+class Game:
+    clock = pg.time.Clock()
+
+    def __init__(self, width, height):
+        self.size = (width, height)
+        self.display = pg.display
+        self.screen = self.display.set_mode(self.size)
+        self.screen.fill((60, 60, 60))
+        self.display.set_caption('Mi juego')
+
+        self.balls = []
+
+        for i in range(random.randrange(5,15)):
+            b = Ball()
+            b.setColor((random.randrange(256), random.randrange(256), random.randrange(256)))
+            b.x = random.randrange(800)
+            b.y = random.randrange(600)
+            b.velocidad = random.randrange(10)
+
+            self.balls.append(b)
+
+
+    def start(self):
+        while True:
+            self.clock.tick(_fps)
+
+            for event in pg.event.get():
+                if event.type == QUIT:
+                    pg.quit()
+                    sys.exit()
+
+            #Modifica la posición de ball
+            for ball in self.balls:
+                ball.avanza()
+
+            #Pintar los sprites en screen
+            self.screen.fill((60,60,60))
+
+            for lolailo in self.balls:
+                self.screen.blit(lolailo, (lolailo.x, lolailo.y))
+
+            self.display.flip()
+
+if __name__ == '__main__':
+    pg.init()
+    game = Game(800, 600)
+    game.start()
